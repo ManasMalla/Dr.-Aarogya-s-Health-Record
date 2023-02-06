f5b611a96a6f0a5d90212bcaf4bd72776400b959
ManasMalla <manasmalla.dev@gmail.com>
2023-02-06 16:38:11 +0530

    • Updating the architecture

--- app/src/main/java/com/manasmalla/draarogyashealthrecord/ui/components/SegmentedControl.kt
+++ app/src/main/java/com/manasmalla/draarogyashealthrecord/ui/components/SegmentedControl.kt
@@ -11,15 +11,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import com.manasmalla.draarogyashealthrecord.ui.theme.GoogleSansFontFamily
 
 @Composable
 fun SegmentedControl(
