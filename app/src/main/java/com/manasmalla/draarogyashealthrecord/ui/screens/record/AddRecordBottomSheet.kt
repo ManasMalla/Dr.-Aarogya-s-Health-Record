0ea4a37c82edeffd6447549fde3597345885139a
Manas Malla <manasmalla.dev@gmail.com>
2023-02-09 20:09:14 +0530

    Fixed the Add Record UI Workflow

--- app/src/main/java/com/manasmalla/draarogyashealthrecord/ui/screens/record/AddRecordBottomSheet.kt
+++ app/src/main/java/com/manasmalla/draarogyashealthrecord/ui/screens/record/AddRecordBottomSheet.kt
@@ -1,6 +1,8 @@
 package com.manasmalla.draarogyashealthrecord.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -10,8 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
@@ -35,6 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.manasmalla.draarogyashealthrecord.model.Metrics
@@ -115,30 +120,34 @@ fun AddRecordBottomSheet(
         Text(text = "Add a record", style = MaterialTheme.typography.titleMedium)
         Text(text = "Here’s a quick overview of all of your records")
         Spacer(modifier = Modifier.height(16.dp))
-        LazyMetricGrid {
-            measurableMetrics.mapIndexed { index, metric ->
-
-                OutlinedTextField(value = measurements[index],
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2), horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            itemsIndexed(measurableMetrics) { index, metric ->
+                OutlinedTextField(
+                    value = measurements[index],
                     onValueChange = {
                         val list = measurements.toMutableList()
                         list[index] = it
                         onUiStateChanged(list)
                     },
                     label = {
-                        Text(text = metric.name.splitCamelCase)
+                        Text(
+                            text = metric.name.splitCamelCase,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     },
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .width(16.dp.times(metric.name.length) + 8.dp),
+                    modifier = Modifier.padding(vertical = 8.dp),
                     keyboardOptions = KeyboardOptions.Default.copy(
                         keyboardType = KeyboardType.Number,
                         imeAction = if (index == measurableMetrics.lastIndex) ImeAction.Done else ImeAction.Next
                     ),
-                    trailingIcon = {
-
-                        Text(text = metric.unit)
-
-                    })
+                    trailingIcon = if (measurements[index].isNotBlank()) trailingMetricIcon(
+                        metric = metric.unit,
+                        visible = measurements[index].isNotBlank()
+                    ) else null
+                )
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
@@ -147,6 +156,13 @@ fun AddRecordBottomSheet(
         ) {
             Text(text = "Add Record")
         }
+    }
+}
+
+@Composable
+fun trailingMetricIcon(metric: String, visible: Boolean) = @Composable {
+    AnimatedVisibility(visible = visible) {
+        Text(text = metric)
     }
 }
 
@@ -197,54 +213,17 @@ fun LazyMetricGrid(modifier: Modifier = Modifier, content: @Composable () -> Uni
 @Composable
 fun AddRecordBottomSheetPreview() {
     DrAarogyasHealthRecordTheme {
-//        AddRecordBottomSheet(
-//            measurableMetrics = listOf(
-//                Metrics.Weight,
-//                Metrics.Calories,
-//                Metrics.FatPercentage,
-//                Metrics.WaterPercentage,
-//                Metrics.MuscleMass,
-//                Metrics.BoneMass,
-//            ), measurements = listOf(
-//                61.2, 1460.0, 44.0, 76.0, 4.5, 3.2
-//            ).map { it.toString() }, addRecordEnabled = true
-//        )
-
-        val measurableMetrics = listOf(
-            Metrics.Weight,
-            Metrics.Calories,
-            Metrics.FatPercentage,
-            Metrics.WaterPercentage,
+        AddRecordBottomSheet(
+            measurableMetrics = listOf(
+                Metrics.Weight,
+                Metrics.Calories,
+                Metrics.FatPercentage,
+                Metrics.WaterPercentage,
+                Metrics.MuscleMass,
+                Metrics.BoneMass,
+            ), measurements = listOf(
+                61.2, "", "", "", "", 3.2
+            ).map { it.toString() }, addRecordEnabled = true
         )
-        val measurements = listOf(
-            61.2, 1460.0, 44.0, 76.0
-        ).map { it.toString() }
-
-        LazyMetricGrid {
-            measurableMetrics.mapIndexed { index, metric ->
-
-                OutlinedTextField(value = measurements[index],
-                    onValueChange = {
-                        val list = measurements.toMutableList()
-                        list[index] = it
-                        //onUiStateChanged(list)
-                    },
-                    label = {
-                        Text(text = metric.name.splitCamelCase)
-                    },
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .width(16.dp.times(metric.name.length) + 8.dp),
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Number,
-                        imeAction = if (index == measurableMetrics.lastIndex) ImeAction.Done else ImeAction.Next
-                    ),
-                    trailingIcon = {
-
-                        Text(text = metric.unit)
-
-                    })
-            }
-        }
     }
 }
